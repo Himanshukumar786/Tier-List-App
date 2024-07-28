eab8302fefe1b268958d0ca05b18b2e44aa4ebfe
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-07-28 19:24:11 +0530

    final changes

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-const tierInput = document.getElementById("Tier");
-console.log(tierInput)
+let currentDraggedItem;
 
-const submitBtn = document.getElementById("submit")
-submitBtn.addEventListener('click', (Event) => {
-    Event.preventDefault();
-    const target = Event.target;
+const tierInput = document.getElementById('tier');
 
-    createTierList()
+const itemContainers = document.getElementsByClassName('item-container');
+
+// const tierLists = document.querySelectorAll('.tier-list');
+
+const submitBtn = document.getElementById('submit');
+
+const imageForm = document.getElementById('image-form');
+
+
+for(const itemContainer of itemContainers) {
+    setUpItemContainerForDrag(itemContainer);
+}
+
+// tierLists.forEach(setUpDropZoneInTierList);
+
+
+imageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const imageItemInput = document.getElementById('image-item');
+    if(imageItemInput.value === '') {
+        alert('Please enter a valid image url');
+        return;
+    }
+    const imageUrl = imageItemInput.value;
+    createTierListItem(imageUrl);
+    imageItemInput.value = '';
+});
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault(); // stops the default behaviour of the event
+    // To get access of the element on which this event was fired
+    if(tierInput.value === '') {
+        alert('Please enter a tier name');
+        return;
+    }
+    createTierList(tierInput.value);
+    tierInput.value = '';
 });
 
 function createTierList(tierListName) {
@@ -29,8 +61,54 @@ function createTierList(tierListName) {
     newTierList.appendChild(heading);
     newTierList.appendChild(newTierListItems);
 
-    // setUpDropZoneInTierListItem(newTierListItems);
+    setUpDropZoneInTierListItem(newTierListItems);
 
     const tierSection = document.getElementById('tier-list-section');
     tierSection.appendChild(newTierList);
+}
+
+function createTierListItem(imageUrl) {
+    const imageDiv = document.createElement('div');
+    imageDiv.setAttribute('draggable', 'true');
+    imageDiv.classList.add('item-container');
+
+    setUpItemContainerForDrag(imageDiv);
+
+    const img = document.createElement('img');
+    img.src = imageUrl;
+
+    imageDiv.appendChild(img);
+
+    const nonTierSection = document.getElementById('non-tier-section');
+    nonTierSection.appendChild(imageDiv);
+
+}
+
+function setUpItemContainerForDrag(itemContainer) {
+    itemContainer.addEventListener('dragstart', (event) => {
+        console.log(event)
+        currentDraggedItem = event.target.parentNode;
+    });
+
+    itemContainer.addEventListener('dblclick', (event) => {
+        const parentNode = event.target.parentNode;
+        const nonTierSection = document.getElementById('non-tier-section');
+        nonTierSection.appendChild(parentNode);
+    });
+}
+
+function setUpDropZoneInTierListItem(tierListItem) {
+    tierListItem.addEventListener('drop', (event) => {
+        event.preventDefault(); // stops the default behaviour of the event which is to not allow drop
+    });
+
+    tierListItem.addEventListener('dragover', function (event) {
+        // console.log(event.target);
+        // event.target.appendChild(currentDraggedItem);
+        console.log("event coming up", event);
+        if(this !== currentDraggedItem.parentNode) {
+            this.appendChild(currentDraggedItem);
+        }
+
+    });
 }
